1135c62a21d23a3dcb266d672fd3daddbf51d087
J-shiro <541737231@qq.com>
2025-01-29 13:16:41 +0800

    win-20250129

--- Linux网络编程/chap7/bind_in.c
+++ Linux网络编程/chap7/bind_in.c
@@ -12,18 +12,30 @@
 
 #define MYPORT 3490
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
     int sockfd;
     struct sockaddr_in my_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd == -1){
+    if (sockfd == -1)
+    {
         perror("socket");
         exit(1);
     }
 
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(MYPORT);
-    my_addr.sin_addr.s_addr = inet_addr("")
+    my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    bzero(&(my_addr.sin_zero), 8); // 将一段内存区域清零
+
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) // 绑定确定通信端点
+    {
+        perror("bind");
+        exit(1);
+    }
+    // 接收和发送数据, 数据处理
+
+    close(sockfd);
 }
